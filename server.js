0690556161532fa639f56619bc4ea54e554d2c6f
Prateek Divyanshu <prateek10023@gmail.com>
2021-02-06 17:10:18 +0530

    Basic server setUp and running...

--- server.js
+++ server.js
@@ -3,6 +3,10 @@ const app = express();
 
 let PORT = 8081;
 
+// Route files 
+app.use('/memes',require('./routes/backendRoutes'));
+
+
 app.listen(PORT, () =>{
   console.log(`Backend Server running successfully on ${PORT}....\n`);
 })
